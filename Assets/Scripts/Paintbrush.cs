9c745e77b9acdff4c8077a7161b8fe44de9b8373
Mustan Pathetic <patheticmustan@gmail.com>
2021-12-04 17:30:11 -0500

    painting minigame demo

---
+++ Assets/Scripts/Paintbrush.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paintbrush : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        // while the brush is showing, it'll paint the scene
+        GetComponent<SpriteRenderer>().enabled = Input.GetMouseButton(0);
+
+        // move to mouse
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        gameObject.transform.position = mousePos;
+    }
+}
---
+++ Assets/disappearWhenTheGameStarts.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class disappearWhenTheGameStarts : MonoBehaviour {
+    void Start() {
+        // to repaint the canvas, set the game object to active!
+        gameObject.SetActive(false);
+    }
+
+    void FixedUpdate() {
+        
+    }
+}
